87647a767196fb163dbe90df9b98c361755d37e3
Bengalistore <krjnstore@gmail.com>
2025-09-16 07:15:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "25750007"))
+API_HASH = os.environ.get("API_HASH", "9707616e3703ade0dec6d951780f9cca")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "7259440235"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,8 +14,8 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002534798811")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002591125289"))
 
 
 # Mongo DB Database Config
